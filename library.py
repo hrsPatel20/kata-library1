4f3c754345cab288a4eb5bb6b9125acf641d8f5b
hrspatel20 <hrspatel2010@gmail.com>
2024-08-11 11:48:48 +0530

    Implement add book feature

---
+++ library.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, isbn, title, author, year):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.year = year
+        self.available = True
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
